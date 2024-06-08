b6cddc13d314a6a7f174efd95c61c30216d17d7f
Vito Medlej <vito.medlej@gmail.com>
2024-06-09 00:20:04 +0300

    ui

--- src/Components/PreLoader.tsx
+++ src/Components/PreLoader.tsx
@@ -13,14 +13,12 @@ import HomeProductsCarousel from './HomeProductsCarousel/HomeProductsCarousel'
 import HomeCategoryList from './HomeCateogryList/HomeCategoryList'
 import FullscreenPoster from './FullscreenPoster/FullscreenPoster'
 import { useCategoriesContext } from '@/context/Contexts'
-import useProducts from '@/Hooks/useProducts'
 
 const 
 PreLoader = ({data,featuredProducts,products,brands,resImages,vids,SectionsRes}:any) => {
   console.log('data: ', data, products);
   const {categories} = useCategoriesContext();
   
-  // const { array1, array2, array3 } = useProducts(data)
   // let videosArray = vids && vids[0]?.videos?.videos || null
   // const router= useRouter();
   // const {text} = useLanguage()
